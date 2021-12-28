025c1eefd61fe6a9afc20539bc72eb8851464490
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-28 12:00:59 -0800

    add test for mercury prototype.method

--- _tests_/User.test.js
+++ _tests_/User.test.js
@@ -7,4 +7,8 @@ describe('User', () => {
     expect(user0.earthAge).toEqual(28);
   });
 
+  test('Should correctly divide user age by Mercury years', () => {
+    const user0 = new User(28);
+    expect(user0.earthAge.mercury()).toEqual(116.6666666667)
+  })
 });
