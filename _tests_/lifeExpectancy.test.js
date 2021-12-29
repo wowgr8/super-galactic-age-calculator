60940ce42a6fe869726952ac7deb87bea95865f1
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-28 17:17:13 -0800

    add initial spec for  life expectancy constructor

---
+++ _tests_/lifeExpectancy.test.js
@@ -0,0 +1,9 @@
+import lifeExpectancy from '../src/js/lifeExpectancy.js';
+
+describe('lifeExpectancy', () => {
+
+  test('Should return how many years a user has left to live', () => {
+    const user0 = new lifeExpectancy(28, 88);
+    expect(user0.currentAge - user0.expectedAge).toEqual(60);
+  });
+});
