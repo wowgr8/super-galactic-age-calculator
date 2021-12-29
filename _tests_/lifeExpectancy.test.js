857e9745f9ff28de7c303ffd559db8247acb0301
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-28 17:21:04 -0800

    add passing spec for life expectancy constructor as well as business logic

--- _tests_/lifeExpectancy.test.js
+++ _tests_/lifeExpectancy.test.js
@@ -4,6 +4,6 @@ describe('lifeExpectancy', () => {
 
   test('Should return how many years a user has left to live', () => {
     const user0 = new lifeExpectancy(28, 88);
-    expect(user0.currentAge - user0.expectedAge).toEqual(60);
+    expect(user0.expectedAge - user0.currentAge).toEqual(60);
   });
 });
---
+++ src/js/lifeExpectancy.js
@@ -0,0 +1,8 @@
+// Determines how many years a user has left to live on each planet… (yikes!) To do this, the application will have to calculate the user's life expectancy. You can determine average life expectancy as you see fit. A simple way to do this would be to have the user input the average life expectancy for their demographic. A more involved way to do this would be to collect other information from the user (e.g. lifestyle, country of residence, activity level, etc.) to determine their life expectancy.
+
+export default class lifeExpectancy {
+  constructor(currentAge, expectedAge) {
+    this.currentAge = currentAge;
+    this.expectedAge = expectedAge;
+  }
+}
