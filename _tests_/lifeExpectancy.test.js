cd1ee1112cc30ca4df1d6d3d79c350dca3f97d93
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2021-12-28 17:49:37 -0800

    add spec for life expectancy in mercury years

--- _tests_/lifeExpectancy.test.js
+++ _tests_/lifeExpectancy.test.js
@@ -3,7 +3,12 @@ import lifeExpectancy from '../src/js/lifeExpectancy.js';
 describe('lifeExpectancy', () => {
 
   test('Should return how many years a user has left to live', () => {
-    const user0 = new lifeExpectancy(28, 88);
+    const user0 = new lifeExpectancy(88, 28);
     expect(user0.expectedAge - user0.currentAge).toEqual(60);
   });
+
+  test('Should return life expectancy in mercury years', () => {
+    const user0 = new lifeExpectancy(88, 28);
+    expect((user0.expectedAge - user0.currentAge)/.24).toEqual(250);
+  })
 });
